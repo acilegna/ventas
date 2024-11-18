92d9c8aed303b2e95ef51dd19d42749a19c02734
acilegna <acilegna.airam88@gmail.com>
2024-11-18 11:21:53 -0600

    update vercel app boostrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,8 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
+//agregdo para configurar vercel
+$middleware->trustProxies(at: '*');
 
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
